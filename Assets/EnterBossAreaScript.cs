550896a5b7ef1e7c93bbfae9fdfe58a43af4d1a7
Gigi Nemsadze <g.nemsadze@altasoft.ge>
2025-04-10 02:19:35 +0400

    mc

--- Assets/EnterBossAreaScript.cs
+++ Assets/EnterBossAreaScript.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class EnterBossAreaScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CameraController cameraController; // Assign this in Inspector
+    public int targetSize = 3; // Zoom value for boss area
+    public float transitionTime = 1f; // Duration of zoom
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Entered Boss Area");
+            if (cameraController != null)
+            {
+                Debug.Log("Calling ZoomTo()");
+                cameraController.ZoomTo(targetSize, transitionTime);
+            }
+            else
+            {
+                Debug.LogWarning("CameraController reference is null.");
+            }
+        }
     }
 }
